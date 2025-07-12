8796b6298ea8bb502c6f9617bbb504c5f220e64a
Prajwal pati <patilprajwal982@gmail.com>
2025-07-12 18:11:33 +0530

    manual way of versioning

---
+++ Web-API-Versioning/V1/Controllers/CountryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_API_Versioning.Models.DTOs;
+
+namespace Web_API_Versioning.V1.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CountryController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var countryDomain = CountryData.Get();
+            var countryDto = new List<CountryDtoV1>();
+
+            foreach (var item in countryDomain)
+            {
+                countryDto.Add(new CountryDtoV1
+                {
+                    Name = item.Name,
+                    Id = item.Id,   
+                });
+            }
+
+            return Ok(countryDto);
+        }
+    }
+}
--- Web-API-Versioning/V2/Controllers/CountryController.cs
+++ Web-API-Versioning/V2/Controllers/CountryController.cs
@@ -1,23 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Web_API_Versioning.Models.DTOs;
 
-namespace Web_API_Versioning.Controllers
+namespace Web_API_Versioning.V2.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v2/[controller]")]
     public class CountryController : Controller
     {
         [HttpGet]
         public IActionResult Get()
         {
             var countryDomain = CountryData.Get();
-            var countryDto = new List<CountryDto>();
+            var countryDto = new List<CountryDtoV2>();
 
             foreach (var item in countryDomain)
             {
-                countryDto.Add(new CountryDto
+                countryDto.Add(new CountryDtoV2
                 {
-                    Name = item.Name,
+                    CountryName = item.Name,
                     Id = item.Id,   
                 });
             }
