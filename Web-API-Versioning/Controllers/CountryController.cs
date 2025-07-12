bc2fbbbefb6d68f68d49893566aa31aa084d2389
Prajwal pati <patilprajwal982@gmail.com>
2025-07-12 18:06:24 +0530

    initial code

---
+++ Web-API-Versioning/Controllers/CountryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_API_Versioning.Models.DTOs;
+
+namespace Web_API_Versioning.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountryController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var countryDomain = CountryData.Get();
+            var countryDto = new List<CountryDto>();
+
+            foreach (var item in countryDomain)
+            {
+                countryDto.Add(new CountryDto
+                {
+                    Name = item.Name,
+                    Id = item.Id,   
+                });
+            }
+
+            return Ok(countryDto);
+        }
+    }
+}
---
+++ Web-API-Versioning/CountryData.cs
@@ -0,0 +1,26 @@
+﻿using Web_API_Versioning.Models.Domain;
+
+namespace Web_API_Versioning
+{
+    public static class CountryData
+    {
+        public static List<Country> Get()
+        {
+            var countryList = new[]
+            {
+                new {Id = 4 , Name = "Germany"},
+                new {Id = 5 , Name = "France"},
+                new {Id = 6 , Name = "Australia"},
+                new {Id = 7 , Name = "Brazil"},
+                new {Id = 8 , Name = "Japan"},
+                new {Id = 9 , Name = "South Africa"},
+                new {Id = 10, Name = "China"},
+                new {Id = 11, Name = "Mexico"},
+                new {Id = 12, Name = "Italy"},
+                new {Id = 13, Name = "Russia"}
+
+            };
+            return countryList.Select(c =>  new Country {Id = c.Id, Name = c.Name}).ToList();
+        }
+    }
+}
