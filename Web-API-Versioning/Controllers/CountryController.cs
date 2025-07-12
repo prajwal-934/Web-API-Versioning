b1a0cf552ab816fc186f89964586842d3b4fe557
Prajwal pati <patilprajwal982@gmail.com>
2025-07-12 18:37:28 +0530

    versioning using nugget pkg

--- Web-API-Versioning/Controllers/CountryController.cs
+++ Web-API-Versioning/Controllers/CountryController.cs
@@ -4,20 +4,43 @@ using Web_API_Versioning.Models.DTOs;
 namespace Web_API_Versioning.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v{version:apiversion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     public class CountryController : Controller
     {
+        
+        [MapToApiVersion("1.0")]
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult GetV1()
         {
             var countryDomain = CountryData.Get();
-            var countryDto = new List<CountryDto>();
+            var countryDto = new List<CountryDtoV1>();
 
             foreach (var item in countryDomain)
             {
-                countryDto.Add(new CountryDto
+                countryDto.Add(new CountryDtoV1
                 {
                     Name = item.Name,
+                    Id = item.Id,   
+                });
+            }
+
+            return Ok(countryDto);
+        }
+
+        [MapToApiVersion("2.0")]
+        [HttpGet]
+        public IActionResult GetV2()
+        {
+            var countryDomain = CountryData.Get();
+            var countryDto = new List<CountryDtoV2>();
+
+            foreach (var item in countryDomain)
+            {
+                countryDto.Add(new CountryDtoV2
+                {
+                    CountryName = item.Name,
                     Id = item.Id,   
                 });
             }
--- Web-API-Versioning/Program.cs
+++ Web-API-Versioning/Program.cs
@@ -10,6 +10,13 @@ namespace Web_API_Versioning
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            builder.Services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+            });
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
